f14b6917dd392fe3a39a6df0f6a8fceb44305e78
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-08-26 18:11:13 +0300

    menubar update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private menu: MenuController) { }
+  currentUrl: string;
 
-  openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
-  }
-
-  openEnd() {
-    this.menu.open('end');
-  }
+  constructor(private menu: MenuController,private router: Router) { }
 
   openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
+    this.menu.enable(true, 'menuBar');
+    this.menu.open('menuBar');
+  }
+  closeCustom(){
+    this.menu.enable(false, 'menuBar');
+    this.menu.close('menuBar');
+  }
+  async url(){
+     this.currentUrl= this.router.url.toString();
+     await console.log(this.currentUrl);
   }
 }
 
