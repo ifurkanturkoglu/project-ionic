f1f9dee1f67312de53083038e781239cf36e1eb8
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-08-27 18:15:44 +0300

    update

--- src/app/modules/pages/groups/groups.page.ts
+++ src/app/modules/pages/groups/groups.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups.page.scss'],
 })
 export class GroupsPage implements OnInit {
-
+  kisi: any[]=['a','b','c'];
+  controller=false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  groupsChange(){
+    this.controller =!this.controller;
+  }
 }
