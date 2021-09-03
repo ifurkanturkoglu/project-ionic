f1b88456d9e4e1334eb0e999bd5d65a67305ead4
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-09-03 14:56:14 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
+import { Network } from '@capacitor/network';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  
+
 
   constructor(private menu: MenuController,private router: Router) { }
 
@@ -17,11 +17,10 @@ export class AppComponent {
     this.menu.open('menuBar');
   }
   closeCustom(){
-    
     this.menu.close('menuBar');
-  }  
+  }
+  network(){
+    const a = Network.getStatus();
+    a.then(res => console.log(res));
+  }
 }
-
-
-
-
