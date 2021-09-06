c0c724f2ea0b6257996c056b85d55fe01993c96e
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-09-06 18:06:02 +0300

    login update

--- src/app/modules/pages/login/login.page.ts
+++ src/app/modules/pages/login/login.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
-
+import { Network } from '@capacitor/network';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  networkAccess:boolean;
   constructor(private menu: MenuController) { }
 
   ngOnInit() {
@@ -16,5 +16,7 @@ export class LoginPage implements OnInit {
   menuDisable(){
     this.menu.enable(false,"menuBar")
   }
-
+  networkCheck(){
+    Network.getStatus().then(a=> {this.networkAccess = a.connected})
+  }
 }
