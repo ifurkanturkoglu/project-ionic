8eb17b17d95074d59b3a16a0bd5554050c3e9d41
Medusjy <fatma.keskin65@erurum.edu.tr>
2021-09-08 14:57:49 +0300

    new component add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { HelpPageModule } from './modules/pages/options/help/help.module';
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -9,35 +11,38 @@ const routes: Routes = [
   },
   {
     path: 'mainpage',
-    loadChildren: () => import('./modules/pages/mainpage/mainpage.module').then( m => m.MainpagePageModule)
+    loadChildren: () => import('./modules/pages/mainpage/mainpage.module').then(m => m.MainpagePageModule)
   },
   {
     path: 'profilepage',
-    loadChildren: () => import('./modules/pages/profilepage/profilepage.module').then( m => m.ProfilepagePageModule)
+    loadChildren: () => import('./modules/pages/profilepage/profilepage.module').then(m => m.ProfilepagePageModule)
   },
   {
     path: 'statspage',
-    loadChildren: () => import('./modules/pages/statspage/statspage.module').then( m => m.StatspagePageModule)
-  },  {
+    loadChildren: () => import('./modules/pages/statspage/statspage.module').then(m => m.StatspagePageModule)
+  },
+  {
     path: 'groups',
-    loadChildren: () => import('./modules/pages/groups/groups.module').then( m => m.GroupsPageModule)
+    loadChildren: () => import('./modules/pages/groups/groups.module').then(m => m.GroupsPageModule)
   },
   {
     path: 'lessons',
-    loadChildren: () => import('./modules/pages/lessons/lessons.module').then( m => m.LessonsPageModule)
+    loadChildren: () => import('./modules/pages/lessons/lessons.module').then(m => m.LessonsPageModule)
   },
   {
     path: 'options',
-    loadChildren: () => import('./modules/pages/options/options.module').then( m => m.OptionsPageModule)
+    loadChildren: () => import('./modules/pages/options/options.module').then(m => m.OptionsPageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./modules/pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./modules/pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'register',
-    loadChildren: () => import('./modules/pages/register/register.module').then( m => m.RegisterPageModule)
-  }
+    loadChildren: () => import('./modules/pages/register/register.module').then(m => m.RegisterPageModule)
+  },
+  
+
 
 ];
 
--- src/app/modules/pages/options/options-routing.module.ts
+++ src/app/modules/pages/options/options-routing.module.ts
@@ -7,7 +7,24 @@ const routes: Routes = [
   {
     path: '',
     component: OptionsPage
-  }
+  },
+  {
+    path: 'password',
+    loadChildren: () => import('./password/password.module').then( m => m.PasswordPageModule)
+  },
+  {
+    path: 'help',
+    loadChildren: () => import('./help/help.module').then( m => m.HelpPageModule)
+  },
+  {
+    path: 'premium',
+    loadChildren: () => import('./premium/premium.module').then( m => m.PremiumPageModule)
+  },
+  {
+    path: 'invite',
+    loadChildren: () => import('./invite/invite.module').then( m => m.InvitePageModule)
+  },
+  
 ];
 
 @NgModule({
